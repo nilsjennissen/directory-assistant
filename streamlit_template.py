5cb40bd29606e52435904bc003610659d6f20c26
nilsjenn <93837106+nilsjenn@users.noreply.github.com>
2023-08-24 17:08:48 +0200

    streamlit_template.py changed

--- streamlit_template.py
+++ streamlit_template.py
@@ -1,4 +1,3 @@
-"""Python file to serve as the frontend"""
 import streamlit as st
 from streamlit_chat import message
 from langchain.chains import ConversationChain
@@ -7,15 +6,7 @@ from credentials import api_key
 import openai
 
 
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0, openai_api_key=api_key)
-    chain = ConversationChain(llm=llm)
-    return chain
-
-chain = load_chain()
-
-# From here down is all the StreamLit UI.
+# ----------------- STREAMLIT APP ----------------- #
 st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
 st.header("LangChain Demo")
 
@@ -26,16 +17,26 @@ if "past" not in st.session_state:
     st.session_state["past"] = []
 
 
+# ----------------- FUNCTIONS ----------------- #
+def load_chain():
+    """Logic for loading the chain you want to use should go here."""
+    llm = OpenAI(temperature=0, openai_api_key=api_key)
+    chain = ConversationChain(llm=llm)
+    return chain
+
+
 def get_text():
     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
     return input_text
 
 
+chain = load_chain()
 user_input = get_text()
 
+# Add a button to submit the user input
+# ----------------- INTERACTIONS ----------------- #
 if user_input:
     output = chain.run(input=user_input)
-
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
 
@@ -44,6 +45,3 @@ if st.session_state["generated"]:
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
-
-    st.session_state["generated"] = []
-    st.session_state["past"] = []
